0691f4b7480a9278377293300383feb2605fc018
beingAkash <beingakki26@gmail.com>
2020-03-30 04:13:11 +0530

    Changed Final Name

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Akash kr'
+        name: 'Akash kumar'
     })
 })
 
